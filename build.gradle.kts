d5330e9d81ceeb046043e7ece26195046e4b373d
Serkan Dağlıoğlu <serkandaglioglu@gmail.com>
2025-05-14 21:12:58 +0300

    - upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 
 plugins {
     id("com.android.application") version "8.9.2" apply false
-    id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.library") version "8.9.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
 
     id("com.google.gms.google-services") version "4.4.2" apply false
 
